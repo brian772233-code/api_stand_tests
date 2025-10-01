f9607a8ee296e1877e14bc9fde529be79a93a3da
Brian Duarte Suarez <brian772233@gmai.com>
2025-09-30 18:50:50 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://cnt-d60d1f7e-6917-4150-8eb6-177c7590386d.containerhub.tripleten-services.com"
+CREATE_USER_PATH = "/api/v1/users"
+USER_TABLE_PATH = "/api/db/resources/user_model.csv"
---
+++ data.py
@@ -0,0 +1,7 @@
+headers = {
+    "Content-Type" : "application/json"
+
+}
+
+user_body = {"firstName": "Andrea","phone": "+1234567890",
+             "address": "123 Elm Street, Hilltop"}
